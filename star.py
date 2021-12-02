712a13c38207854594e5db43faae4db81d91ec7e
Sakshi7890 <95436616+Sakshi7890@users.noreply.github.com>
2021-12-02 22:56:15 +0530

    Star project of Sakshi Verma

---
+++ star.py
@@ -0,0 +1,8 @@
+# imorting turtle in myturtle name
+import turtle as myturtle
+myturtle.begin_fill()
+myturtle.fillcolor("red")
+for i in range(5):          #loop
+    myturtle.forward(100)
+    myturtle.right(144)
+myturtle.end_fill()
